414057f0ba40b5151679aa7ad6d92d2ecae3d973
NakedM <khmchris@naver.com>
2014-07-17 17:22:04 +0900

    Revert "Revert "Revert "notebook + desktop"""
    This reverts commit dd05c4dd58aff5d0840f7cb575084e7261094c7a.

--- algoPro/algospot_alltest/algospot_fence.cpp
+++ algoPro/algospot_alltest/algospot_fence.cpp
@@ -1,66 +1,64 @@
-#include<iostream>
-#include<fstream>
-#include<algorithm>
-#include<vector>
-
-#pragma warning(disable:4996)
-
-using namespace std;
-
-
-vector<int> heights;
-
-
-int solveprob(int low, int high){
-	if (low == high)
-		return heights[low];
-
-	int mid = (low + high) / 2;
-
-	int ret = max(solveprob(low, mid), solveprob(mid + 1, high));
-
-	int le = mid, ri = mid + 1;
-
-	int h = min(heights[le], heights[ri]);
-
-	ret = max(ret, h * 2);
-
-	while (low < le || ri < high)
-	{
-		if (ri < high && (low == le || heights[le-1] < heights[ri+1]))
-		{
-			ri++;
-			h = min(h, heights[ri]);
-		}
-		else{
-			le--;
-			h = min(h, heights[le]);
-		}
-
-		ret = max(ret, h * (ri - le + 1));
-	}
-	return ret;
-}
-	
-
-
-int main(){
-#ifdef _CONSOLE
-	freopen("input.txt", "r", stdin);
-#endif
-
-	int tc;
-	cin >> tc;
-	while (tc--)
-	{
-		int n;
-		cin >> n;
-		heights = vector<int>(n, 0);
-		for (int i = 0; i < n; i++)
-			cin >> heights[i];
-
-		cout << solveprob(0, n - 1) << endl;
-	}
-
-	return 0;
-}
+//#include<iostream>
+//#include<fstream>
+//#include<string>
+//#include<cstring>
+//#include<cmath>
+//#include<vector>
+//#include<algorithm>
+//#include<queue>
+//#include<limits>
+//#include<bitset>
+//using namespace std;
+//
+//ifstream in("input.txt");
+//
+//int n;
+//vector<int> vt;
+//
+//int fence(int left, int right){
+//	if(left == right)
+//		return vt[left];
+//
+//	int mid = (left+right)/2;
+//	int rst = max(fence(left,mid),fence(mid+1,right));
+//
+//	int l = mid, r= mid+1;
+//	int height = min(vt[l],vt[r]);
+//
+//	rst = max(rst,height*2);
+//
+//	while(left<l || right>r){
+//		if(left<l && (right == r || vt[l-1] > vt[r+1])){
+//			l--;
+//			height = min(height,vt[l]);
+//		}
+//		else{
+//			r++;
+//			height = min(height,vt[r]);
+//		}
+//		rst = max(rst,height*(r-l+1));
+//	}
+//	return rst;
+//}
+//
+//int main(){
+//#ifdef _CONSOLE    
+//	freopen("input.txt","r", stdin);
+//		freopen("output.txt","w+", stdout);
+//#endif
+//	int tc;
+//	cin>>tc;
+//	while(tc--){
+//		cin>>n;
+//
+//		int a;
+//		for(int i=0;i<n;i++){
+//			cin>>a;
+//			vt.push_back(a);
+//		}
+//
+//		cout<<fence(0,vt.size()-1)<<endl;
+//		vt.clear();
+//	}
+//	return 0;
+//}
